18b9d776105fc48a70567369aa210adc5f53ad91
satya <satya.iton@gmail.com>
2019-06-19 22:35:12 +0530

    FeignClient

---
+++ Fiegn Client/FiegnClient/FiegnClient/src/main/java/com/helpezee/microservices/FiegnClient/FiegnClientApplication.java
@@ -0,0 +1,13 @@
+package com.helpezee.microservices.FiegnClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiegnClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiegnClientApplication.class, args);
+	}
+
+}
---
+++ Fiegn Client/FiegnClient/src/main/java/com/helpezee/microservices/FiegnClient/Employee.java
@@ -0,0 +1,69 @@
+package com.helpezee.microservices.FiegnClient;
+
+import java.util.Date;
+
+public class Employee {
+
+	private int empno;
+
+	@Override
+	public String toString() {
+		return "Employee [empno=" + empno + ", name=" + name + ", salary=" + salary + ", com=" + com + ", project="
+				+ project + ", bdate=" + bdate + "]";
+	}
+
+	private String name;
+	private double salary;
+	private double com;
+	private String project;
+	private Date bdate;
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public double getCom() {
+		return com;
+	}
+
+	public void setCom(double com) {
+		this.com = com;
+	}
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	public Date getBdate() {
+		return bdate;
+	}
+
+	public void setBdate(Date bdate) {
+		this.bdate = bdate;
+	}
+
+}
